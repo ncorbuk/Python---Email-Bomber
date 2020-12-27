a95a94c13bec2e37353098a0ef3238b6e77b1d3c
c4f3firstt <59329439+c4f3firstt@users.noreply.github.com>
2020-12-27 19:33:31 -0300

    Create ReadMeForInstaller.sh

---
+++ ReadMeForInstaller.sh
@@ -0,0 +1,33 @@
+# Python3 Tutorial - || Create a EMAIL Bomb/Bomber || Code Walk-through || Hacking/Info-Sec ||
+
+*#python3* *#email* *#bomb* *#bomber* *#code* *#walk-through* *#hacking* *#info-sec* *#security* *#mass* *#spam* *#create*
+*#how* *#to* *#code* *#script* *#kiddie*
+
+
+![alt text](https://raw.githubusercontent.com/ncorbuk/Python---Email-Bomber/master/Email-Bomber%20v1.0%20Picture.jpg)
+
+## Code Walk-through & Learning resources links
+*If you are using a GMAIL or YAHOO account, you will need to change the settings to "Less Secure Apps" to allow python to send email using there servers, also may need to do that for OUTLOOK, or anyother EMAIL SERVER you use.... You may have to check.*
+
+**YouTube tutorial: https://bit.ly/2Gxea9B**
+
+**For python tutorials: https://bit.ly/2U58Lt9**
+
+**Python_AND_Hacking subreddit: https://bit.ly/2Uf3gbw**
+
+## Installation Section
+
+Instructions:
+```
+1. git clone https://github.com/ncorbuk/Python---Email-Bomber
+2. cd Python---Email-Bomber/
+3. chmod +x Installer.sh
+4. ./Installer.sh
+```
+
+## Become a Patreon - support the channel
+*Become a Patreon and support this channel, so that it can keep creating great content in the future* **LINK: https://www.patreon.com/w3w3w3**
+
+## Built With
+
+* **Python 3.6.5** - [https://www.python.org/](https://www.python.org/)
